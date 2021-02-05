3fc69f7694e411275cbb1488a45e0e5b26bb0ec2
vivimortensen <vivi4531@stud.kea.dk>
2021-02-05 15:32:34 +0100

    start, clickCalculate, readFkrstNumber and readSecondNumber functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+//Setting up
+//Load Site
+window.addEventListener("load", start);
+
+function start(){
+    console.log("Lortet virker");
+    clickCalculate();
+}
+
+function clickCalculate(){
+    console.log("Det bliver klikket på calculate");
+
+    document.querySelector("#calculate").addEventListener("click", readFirstNumber);
+}
+
+function readFirstNumber(){
+    console.log("Første tal læst");
+
+    //Read first number field 
+    const firstNumber = document.querySelector("#firstnumber").value;
+
+    readSecondNumber();
+}
+
+function readSecondNumber(){
+    console.log("Andet tal læst");
+
+    //Read second number field 
+    const secondNumber = document.querySelector("#secondnumber").value;
+
+}

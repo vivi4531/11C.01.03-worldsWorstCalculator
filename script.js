a61ac8683fb7ffa61f67cbafb056794c8fe9f1ba
vivimortensen <vivi4531@stud.kea.dk>
2021-02-06 12:45:01 +0100

    add, sub, mul, div functions etc.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 "use strict";
 
-let resultat; 
-
-//setting up
+//Setting up - page loading
 window.addEventListener("load", start);
 
 function start() {
@@ -30,21 +28,72 @@ function readSecondNumber() {
 }
 
 function readOperator() {
-  //collect numbers
+  //Collect numbers and read operator
   console.log("read operator");
   const firstInputNumber = readFirstNumber();
   const secondInputNumber = readSecondNumber();
-  calculationPlus(firstInputNumber, secondInputNumber);
+const operator = document.querySelector("#operator").value;
+
+if (operator == "add"){
+  calculationAdd(firstInputNumber, secondInputNumber);
+} 
+else if (operator == "sub"){
+  calculationSub(firstInputNumber, secondInputNumber);
 }
 
-function calculationPlus(firstnumber, secondnumber) {
+else if (operator == "mul"){
+  calculationMul(firstInputNumber, secondInputNumber);
+}
+
+else if (operator == "div"){
+  calculationDiv(firstInputNumber, secondInputNumber);
+}
+
+}
+
+function calculationAdd(firstnumber, secondnumber) {
   //takes the numbers fra readOperator and add them
   const resultat = firstnumber + secondnumber;
   console.log(resultat);
 
-  writeResultIntoFirstNumberField();
+  writeResultIntoFirstNumberField(resultat);
 }
 
-function writeResultIntoFirstNumberField(){
-   document.querySelector("#firstnumber").textContent = resultat; 
+function calculationSub(firstnumber, secondnumber) {
+  //takes the numbers fra readOperator and add them
+  const resultat = firstnumber - secondnumber;
+  console.log(resultat);
+
+  writeResultIntoFirstNumberField(resultat);
+}
+
+function calculationMul(firstnumber, secondnumber) {
+  //takes the numbers fra readOperator and add them
+  const resultat = firstnumber * secondnumber;
+  console.log(resultat);
+
+  writeResultIntoFirstNumberField(resultat);
+}
+
+function calculationDiv(firstnumber, secondnumber) {
+  //takes the numbers fra readOperator and add them
+  const resultat = firstnumber / secondnumber;
+  console.log(resultat);
+
+  writeResultIntoFirstNumberField(resultat);
+}
+
+function writeResultIntoFirstNumberField(resultat){
+  //Creates new <li>
+  let listElement = document.createElement("li");
+  //Prepares element with createTextNode and turns resultat into string
+  let createListString = document.createTextNode(resultat.toString());
+  //Place new list element in child
+  listElement.appendChild(createListString);
+  //Place new list element in <ul> results list
+  document.getElementById("results").appendChild(listElement);
+  //Scroll to end of results list
+  document.querySelector("#results").scrollTo(0,10000);
+  //Insert result in firstnumber field
+  document.querySelector("#firstnumber").value = resultat.toString(); 
 }
